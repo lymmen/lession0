e63c8b7fb0904e8d15f21919bbd40fca0dd4cc03
lymmen <lx9087061555@gmail.com>
2024-11-19 21:10:31 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+course_name = "Python"
+time_for_exercise = hours / homeworks
+print("Курс:", course_name, "Всего задач:", homeworks, "Затрачено часов:", hours, "Среднее время выполнения:", time_for_exercise, "часов")
